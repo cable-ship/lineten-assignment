ba30fca1c1eb95d62213c4835e9ef4978c479518
cable-ship <0xnikhilmukati@gmail.com>
2025-11-25 18:14:00 +0530

    added oidc module to create SA for github actions

--- infra/terraform/modules/oidc/variables.tf
+++ infra/terraform/modules/oidc/variables.tf
@@ -1,1 +1,22 @@
 
+variable "project_id" {
+  type = string
+}
+
+variable "pool_id" {
+  type = string
+}
+
+variable "provider_id" {
+  type = string
+}
+
+variable "service_account_id" {
+  description = "Full service account resource ID (e.g. projects/PROJECT_ID/serviceAccounts/SA_NAME@PROJECT_ID.iam.gserviceaccount.com)"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository in format owner/repo"
+  type        = string
+}
